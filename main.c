37b5d225e8d1fb31bacc20ac56c400bbbac79d01
tim-daria <darya.timofeeva.97@gmail.com>
2025-02-05 20:40:23 +0100

    Sorted but not correct

--- main.c
+++ main.c
@@ -12,18 +12,30 @@
 
 #include "push_swap.h"
 #include <stdio.h>
-static void	ft_push(t_list **dst, void *num)
+static void	rotate_clockwise(t_list **stack_b, t_list **last)
 {
-	//t_list	*last;
+	*stack_b = (*stack_b)->next;
+	*last = (*last)->next;
+}
+static void	ft_push_tob(t_list **dst, t_list **last, void *num)
+{
+	//t_list	*last = NULL;
 
 	if (*dst == NULL)
 	{
 		*dst = ft_lstnew(num);
-		//last = *dst;
+		//(*dst)->next = *dst;
+		*last = *dst;
 	}
 	else
+	{
 		ft_lstadd_front(dst, ft_lstnew(num));
-	//last->next = *dst;
+		printf("no");
+		fflush(0);
+		(*last)->next = *dst;
+		printf("yes");
+		fflush(0);
+	}
 }
 static void	*ft_pop(t_list **src)
 {
@@ -38,17 +50,78 @@ static void	*ft_pop(t_list **src)
 }
 static void	transfer(t_list **stack_a, t_list **stack_b)
 {
-	//t_list	*last;
-	//t_list	*start;
 	void	*num;
+	int		count;
+	void	*max;
+	void	*min;
+	t_list	*last;
 
-	//start = *stack_a;
+	max = (*stack_a)->content;
+	min = (*stack_a)->content;
+	last = NULL;
+	count = 0;
+	printf("b");
 	while (*stack_a != NULL)
 	{
 		num = ft_pop(stack_a);
-		ft_push(stack_b, num);
-		//start = start->next;
+		printf("%d ", *(int*)num);
+		fflush(0);
+		count++;
+		//size = ft_lstsize(*stack_b);
+		printf("%d", count);
+		// if (*(int*)num > *(int*)max)
+		// 	max = num;
+		if (count <= 2)
+		{
+			printf("a");
+			fflush(0);
+			ft_push_tob(stack_b, &last, num);
+		}
+		else
+		 {
+			printf("c");
+			fflush(0);
+			if ((*(int*)num > *(int*)max && (*stack_b)->content == max) ||
+				(*(int*)num < *(int*)min && (*stack_b)->content == min))
+			{
+				ft_push_tob(stack_b, &last, num);
+				printf("where");
+				fflush(0);
+			}
+			else if (*(int*)num > *(int*)max && (*stack_b)->content != max)
+			{
+				printf("I");
+				fflush(0);
+				while ((*stack_b)->content != max)
+					rotate_clockwise(stack_b, &last);
+				ft_push_tob(stack_b, &last, num);
+			}
+			else if (*(int*)num < *(int*)min && (*stack_b)->content != min)
+			{
+				while ((*stack_b)->content != min)
+					rotate_clockwise(stack_b, &last);
+				ft_push_tob(stack_b, &last, num);
+			}
+			else
+			//if (*(int*)num < *(int*)max)
+			{
+				printf("am");
+				fflush(0);
+				while (*(int*)num > *(int*)(*stack_b)->content)
+					rotate_clockwise(stack_b, &last);
+				ft_push_tob(stack_b, &last, num);
+			}
+		}
+		if (*(int*)num > *(int*)max)
+			max = num;
+		printf("%d - max\n", *(int*)max);
+		fflush(0);
+		if (*(int*)num < *(int*)min)
+			min = num;
+		printf("%d - min\n ", *(int*)min);
+		fflush(0);
 	}
+	last->next = NULL;
 }
 static int	ft_arraylen(char **str_array)
 {
@@ -95,6 +168,7 @@ int	main(int argc, char **argv)
 	{
 		t_list	*stack_a;
 		t_list	*stack_b;
+		// t_list	*temp; // todo remove
 
 		stack_a = NULL;
 		stack_b = NULL;
@@ -106,7 +180,7 @@ int	main(int argc, char **argv)
 		// 	printf("%d ", *(int *)stack_a->content);
 		// 	stack_a = stack_a->next;
 		// }
-		printf("\n");
+		// printf("\n");
 		transfer(&stack_a, &stack_b);
 		while (stack_a != NULL)
 		{
@@ -114,9 +188,14 @@ int	main(int argc, char **argv)
 			stack_a = stack_a->next;
 		}
 		printf("\n");
+		// while (stack_b->next != stack_b)
 		while (stack_b != NULL)
 		{
 			printf("%d ", *(int *)stack_b->content);
+			fflush(0);
+			// temp = stack_b->next;
+			// ft_lstdelone(stack_b, free);
+			// stack_b = temp;
 			stack_b = stack_b->next;
 		}
 		printf("\n");
